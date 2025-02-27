183a11a9e8819920e63bc099c364bccba8d4381d
SkrilpGit <jessica.smith@futuregen.net.au>
2025-02-27 18:10:16 +1100

    starting to move the recoil stuff onto the camera pivot so you don't losing mouse input while recovering from recoil, or get animation bugs. :(

--- Scenes/recoil_bones.gd
+++ Scenes/recoil_bones.gd
@@ -1,13 +1,5 @@
 extends Node3D
 
-@export var anim_ctrl = AnimationPlayer.new()
-@export var spine_ik = SkeletonIK3D.new()
-@export var left_hand : BoneAttachment3D
-@export var right_hand : BoneAttachment3D
-
-@export var anim_speed : float
-
-var anim_queue = []
 var skeleton = Skeleton3D
 var recoil_bones = Node3D
 var recoil_offset_pos = Vector3.ZERO
@@ -19,6 +11,7 @@ var char = Node3D
 var fired = false
 
 var spine_target
+var spine_ik
 var last_bone_transforms = {}
 
 signal recoil_recovered()
@@ -28,6 +21,7 @@ func _ready():
 	char = get_node("../..")
 	skeleton = $Armature/Skeleton3D
 	recoil_bones = char.find_child("Recoil Bones")
+	spine_ik = skeleton.find_child("Spine_IK")
 	recoil_offset_pos = recoil_bones.position
 	recoil_offset_rot = recoil_bones.rotation
 	print(recoil_bones.get_parent())
@@ -35,10 +29,6 @@ func _ready():
 	init_IK_Tars()
 	#print("spine ",spine_target)
 	#print(get_node(spine_ik.target_node))
-	if left_hand.get_child_count() > 0:
-		char.equipped = left_hand.get_children()[0]
-	elif right_hand.get_child_count() > 0:
-		char.equipped = right_hand.get_children()[0]
 
 func init_IK_Tars():
 	spine_target = char.find_child("IK_target")
@@ -119,30 +109,3 @@ func S_IK(start):
 		spine_ik.stop()
 		skeleton.find_child("Arm_IK_R").stop()
 		skeleton.find_child("Arm_IK_L").stop()
-
-func move_forward():
-	if anim_ctrl.current_animation != "movement_anims/move_forward_standing":
-		anim_ctrl.play("movement_anims/move_forward_standing")
-		anim_ctrl.speed_scale = anim_speed
-
-func move_backward():
-	if anim_ctrl.current_animation != "movement_anims/move_forward_standing":
-		anim_ctrl.play("movement_anims/move_forward_standing")
-		anim_ctrl.seek(anim_ctrl.current_animation_length, true)
-		anim_ctrl.speed_scale = -anim_speed
-
-func move_right():
-	if anim_ctrl.current_animation != "movement_anims/move_right_standing":
-		anim_ctrl.play("movement_anims/move_right_standing")
-		anim_ctrl.speed_scale = anim_speed
-
-func move_left():
-	if anim_ctrl.current_animation != "movement_anims/move_left_standing":
-		anim_ctrl.play("movement_anims/move_left_standing")
-		anim_ctrl.speed_scale = anim_speed
-
-func stand_still():
-	anim_ctrl.play("movement_anims/idle_standing")
-
-func _on_animation_finished(_anim_name):
-	pass 
