b9c0785ff626314d6b0a871900a06875e39c593b
SkrilpGit <jessica.smith@futuregen.net.au>
2025-02-27 16:41:40 +1100

    stable version, many lines of code, working as expected :)

--- Scripts/Characters/Player/basic_guy.gd
+++ Scripts/Characters/Player/basic_guy.gd
@@ -10,19 +10,32 @@ extends Node3D
 var anim_queue = []
 var skeleton = Skeleton3D
 var recoil_bones = Node3D
-var recoil_offset = Vector3.ZERO
+var recoil_offset_pos = Vector3.ZERO
+var recoil_offset_rot = Vector3.ZERO
+var sIKoffset_pos = Vector3.ZERO
+var sIKoffset_rot = Vector3.ZERO
 var char = Node3D
 
 var fired = false
+
+var spine_target
+var last_bone_transforms = {}
 
 func _ready():
 	
 	char = get_node("../..")
 	skeleton = $Armature/Skeleton3D
 	recoil_bones = $"Armature/Skeleton3D/Recoil Bones"
-	recoil_offset = recoil_bones.position
-	print(recoil_offset)
+	recoil_offset_pos = recoil_bones.position
+	recoil_offset_rot = recoil_bones.rotation
+	#print(recoil_offset)
 	
+	spine_target = char.find_child("IK_target")
+	sIKoffset_pos = spine_target.global_position
+	sIKoffset_rot = spine_target.rotation
+	spine_ik.target_node = spine_target.get_path()
+	print("spine ",spine_target)
+	print(get_node(spine_ik.target_node))
 	if left_hand.get_child_count() > 0:
 		char.equipped = left_hand.get_children()[0]
 	elif right_hand.get_child_count() > 0:
@@ -36,40 +49,54 @@ func init_recoil():
 	var hand_l_transform = skeleton.get_bone_global_pose(skeleton.find_bone("Hand.L"))
 	var hand_l = skeleton.find_child("Arm_IK_Tar_L")
 	hand_l.transform = hand_l_transform
-	
-	var head_transform = skeleton.get_bone_global_pose(skeleton.find_bone("Head"))
-	var head = skeleton.find_child("Head_IK_Tar")
-	head.transform = head_transform
-	
 	skeleton.find_child("Arm_IK_R").start()
 	skeleton.find_child("Arm_IK_L").start()
-	skeleton.find_child("Head_IK").start()
-	
-	pass
 
 func _process(delta):
 	if fired:
-		if recoil_bones.position.distance_to(recoil_offset) > 0.05:
-			var dir = recoil_bones.position - recoil_offset
-			recoil_bones.position -= dir * 10*delta
-		elif recoil_bones.position.distance_to(recoil_offset) <= 0.05:
+		if recoil_bones.position.distance_to(recoil_offset_pos) > 0.05:
+			var dir = recoil_bones.position - recoil_offset_pos
+			var dir2 = spine_target.position - sIKoffset_pos
+			dir2 = dir2.normalized()
+			dir = dir.normalized()
+			recoil_bones.position -= dir * 0.75*delta
+			spine_target.position += dir2 * 1.5*delta
+			recoil_bones.rotation = lerp(recoil_bones.rotation,recoil_offset_rot,10*delta)
+			spine_target.rotation = lerp(spine_target.rotation,sIKoffset_rot,10*delta)
+		elif recoil_bones.position.distance_to(recoil_offset_pos) <= 0.05:
 			fired = false
-			skeleton.find_child("Arm_IK_R").set_interpolation(0.0)
-			skeleton.find_child("Arm_IK_L").set_interpolation(0.0)
-			skeleton.find_child("Head_IK").set_interpolation(0.0)
-			skeleton.find_child("Head_IK").stop()
-			skeleton.force_update_all_bone_transforms()
-			pass
+			spine_target.position = Vector3.ZERO
+			spine_target.rotation = sIKoffset_rot
+			recoil_bones.rotation = recoil_offset_rot
+			call_deferred("stop_recoil_IK")
+			
 	pass
 func recoil(dir: Vector3,force: float):
 	#print(dir)
 	#print(recoil_bones.position)
-	recoil_bones.global_position += dir * force
+	var rot = calculate_recoil_rot(dir,force)
+	recoil_bones.global_position += dir * force/2
+	spine_target.global_position += dir * force
+	spine_target.rotation_degrees += Vector3(rot.x,-rot.y,rot.z)*10
+	recoil_bones.rotation_degrees += rot
 	skeleton.find_child("Arm_IK_R").set_interpolation(1.0)
 	skeleton.find_child("Arm_IK_L").set_interpolation(1.0)
-	skeleton.find_child("Head_IK").set_interpolation(1.0)
 	fired = true
 	pass
+
+func calculate_recoil_rot(dir,mag):
+	var rot_y = 0
+	var rot_x = 0
+	
+	var dif = dir - position
+	rot_y = dif.normalized().length() * mag
+	rot_x = -mag
+	var final_rot = Vector3(rot_x,rot_y,0)
+	return final_rot
+
+func stop_recoil_IK():
+	skeleton.find_child("Arm_IK_R").set_interpolation(0.0)
+	skeleton.find_child("Arm_IK_L").set_interpolation(0.0)
 
 func S_IK(start):
 	if start:
@@ -78,7 +105,6 @@ func S_IK(start):
 		spine_ik.stop()
 		skeleton.find_child("Arm_IK_R").stop()
 		skeleton.find_child("Arm_IK_L").stop()
-		skeleton.find_child("Head_IK").stop()
 
 func move_forward():
 	if anim_ctrl.current_animation != "movement_anims/move_forward_standing":
